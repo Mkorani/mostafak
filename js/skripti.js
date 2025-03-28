a069389bbbb3cb95cb8236335b0b56c6b4ec9bd9
mostafak <mostafaa.baggio@gmail.com>
2025-03-28 13:04:42 +0200

    01_viikko

--- js/skripti.js
+++ js/skripti.js
@@ -1,2 +1,32 @@
 console.log("tämä on javascript-testi.");
 alert("moi");
+
+const muuttuja = "hello world";
+
+function funktio () {
+let p =5*6;
+let x, y, z;
+x = 5
+y = 2
+z = x + y;
+            
+console.log("this is z: ", z);
+
+let toinen ="tänne tulee arvo";
+console.log(toinen)
+
+
+
+
+console.log(toinen)
+
+let age = 35;
+let dob = 1989
+let current_year = age + dob;
+
+console.log ("this year is", current_year);
+
+
+}
+//kutsu funktio näin:
+funktio();
